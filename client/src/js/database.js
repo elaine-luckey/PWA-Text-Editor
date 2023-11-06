ecaf3ed63c2770cb1194ba84340827d84908b66f
elaine-luckey <emarieluckey@yahoo.com>
2023-11-06 16:00:16 -0500

    edits based off of instructions in the starter code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,46 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const initdb = async () => {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
+};
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const text = jateDb.transaction('jate', 'readwrite');
+  const store = text.objectStore('jate');
+  const request = store.put({ value: content });
+  const contents = await request;
+
+  if (contents) {
+    console.log("The data has been saved to the IndexedDB", contents);
+  } else {
+    console.error("putDb not implemented");
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const text = jateDb.transaction('jate', 'readonly');
+  const store = text.objectStore('jate');
+  const request = store.getAll();
+  const contents = await request;
+  
+  if (contents) {
+    console.log('Retrieving all of the content from the IndexedDB', contents);
+  } else {
+    console.error("getDb not implemented");
+  }
+}
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
+// Variable for storing beforeinstallprompt event
+let deferredPrompt;
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', async (event) => {
+    deferredPrompt = event;
+    butInstall.style.display = 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const result = await deferredPrompt.userChoice;
+        if (result.outcome === 'accepted') {
+            console.log('PWA installation accepted');
+        }
+        console.log(deferredPrompt);
+        deferredPrompt = null;
+        butInstall.style.display = 'none';
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', () => {
+    console.log('PWA was installed successfully');
+});
